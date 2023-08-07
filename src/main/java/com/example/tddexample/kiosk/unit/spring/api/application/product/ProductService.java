a1af12d492d7ffc6d6116d473beb4e0c5c099d6f
hyunho058 <hyunho058@naver.com>
2023-08-07 23:29:05 +0900

    상품 저장 기능 개발(application) tdd

--- src/main/java/com/example/tddexample/kiosk/unit/spring/api/application/product/ProductService.java
+++ src/main/java/com/example/tddexample/kiosk/unit/spring/api/application/product/ProductService.java
@@ -7,10 +7,12 @@ import com.example.tddexample.kiosk.unit.spring.domain.product.ProductRepository
 import com.example.tddexample.kiosk.unit.spring.domain.product.ProductSellingStatus;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
+@Transactional(readOnly = true)
 @RequiredArgsConstructor
 @Service
 public class ProductService {
@@ -24,10 +26,28 @@ public class ProductService {
             .collect(Collectors.toList());
     }
 
-    public void createProduct(ProductCreateRequest request) {
+    @Transactional
+    public ProductResponse createProduct(ProductCreateRequest request) {
         //productNumber
         //001, 002, 003
         //DB에서 마지막 저장된 product의 상품번호 조회해서 +1
-        productRepository.findLatestProduct();
+        String latestProductNumber = productRepository.findLatestProduct();
+
+        String nextProductNumber = createProductNumber(latestProductNumber);
+
+        Product newProduct = request.toEntity(nextProductNumber);
+        Product savedProduct = productRepository.save(newProduct);
+
+        return new ProductResponse(savedProduct);
+    }
+
+    private String createProductNumber(String latestProductNumber) {
+        if (latestProductNumber == null){
+            return "001";
+        }
+
+        int latestProductNumberInt = Integer.parseInt(latestProductNumber);
+        int nextProductNumberInt = latestProductNumberInt + 1;
+        return String.format("%03d", nextProductNumberInt);
     }
 }
--- src/main/java/com/example/tddexample/kiosk/unit/spring/api/presentation/product/request/ProductCreateRequest.java
+++ src/main/java/com/example/tddexample/kiosk/unit/spring/api/presentation/product/request/ProductCreateRequest.java
@@ -1,9 +1,19 @@
 package com.example.tddexample.kiosk.unit.spring.api.presentation.product.request;
 
+import com.example.tddexample.kiosk.unit.spring.domain.product.Product;
 import com.example.tddexample.kiosk.unit.spring.domain.product.ProductSellingStatus;
+import com.example.tddexample.kiosk.unit.spring.domain.product.ProductType;
 
-public record ProductCreateRequest(String productNumber,
+public record ProductCreateRequest(ProductType type,
                                    ProductSellingStatus sellingStatus,
                                    String name,
                                    int price) {
+    public Product toEntity(String nextProductNumber) {
+        return new Product(
+            nextProductNumber,
+            this.type(),
+            this.sellingStatus(),
+            this.name(),
+            this.price());
+    }
 }
