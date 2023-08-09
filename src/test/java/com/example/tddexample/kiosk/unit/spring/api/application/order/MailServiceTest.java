668139fef33627fbbed847078d45d74f400980cd
Hyunho Kim <hyunho058@naver.com>
2023-08-09 23:55:28 +0900

    Mockito를 활용한 테스트(Mocking) (#3)
    * 순수 mockito로 테스트 작성
    
    * 순수 mockito로 테스트 작성 리팩터링(annotation을 활용한 mock객체)
    
    * 순수 mockito로 테스트 작성 리팩터링(BDDMockito 사용)

--- src/test/java/com/example/tddexample/kiosk/unit/spring/api/application/order/MailServiceTest.java
+++ src/test/java/com/example/tddexample/kiosk/unit/spring/api/application/order/MailServiceTest.java
@@ -1,7 +1,16 @@
 package com.example.tddexample.kiosk.unit.spring.api.application.order;
 
+import com.example.tddexample.kiosk.unit.spring.client.mail.MailSendClient;
+import com.example.tddexample.kiosk.unit.spring.domain.history.mail.MailSendHistory;
+import com.example.tddexample.kiosk.unit.spring.domain.history.mail.MailSendHistoryRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.BDDMockito;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
@@ -10,30 +19,62 @@ import org.springframework.transaction.annotation.Transactional;
 import java.time.LocalDate;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 
-@ActiveProfiles("test")
-@SpringBootTest
-@Transactional
+//@ActiveProfiles("test")
+//@SpringBootTest
+//@Transactional
+@ExtendWith(MockitoExtension.class)
 class MailServiceTest {
-    @Autowired
+//    @Autowired
+//    private MailService mailService;
+//
+//    @DisplayName("메일 정송 성공하면 TRUE를 반환한다.")
+//    @Test
+//    void test() {
+//        //given
+//        LocalDate orderDate = LocalDate.of(2023, 8, 9);
+//        int ordersTotalPrice = 200000;
+//
+//        String fromEmail = "no-replay@cafekiosk.com";
+//        String toEmail = "tet@test.com";
+//        String subject = String.format("[총 매출] %s", orderDate);
+//        String content = String.format("내용 총 매출 합계 = %s", ordersTotalPrice);
+//
+//        //when
+//        boolean result = mailService.send(fromEmail, toEmail, subject, content);
+//
+//        //then
+//        assertThat(result).isTrue();
+//    }
+
+    @Mock
+    private MailSendClient mailSendClient;
+    @Mock
+    private MailSendHistoryRepository mailSendHistoryRepository;
+    @InjectMocks
     private MailService mailService;
 
-    @DisplayName("메일 정송 성공하면 TRUE를 반환한다.")
+    @DisplayName("메일 전송 테스트")
     @Test
-    void test() {
+    void sendMail(){
         //given
-        LocalDate orderDate = LocalDate.of(2023, 8, 9);
-        int ordersTotalPrice = 200000;
+//        MailSendClient mailSendClient = mock(MailSendClient.class);
+//        MailSendHistoryRepository mailSendHistoryRepository = mock(MailSendHistoryRepository.class);
 
-        String fromEmail = "no-replay@cafekiosk.com";
-        String toEmail = "tet@test.com";
-        String subject = String.format("[총 매출] %s", orderDate);
-        String content = String.format("내용 총 매출 합계 = %s", ordersTotalPrice);
+//        MailService mailService = new MailService(mailSendClient, mailSendHistoryRepository);
+
+//        Mockito.when(mailSendClient.send(any(String.class), any(String.class), any(String.class), any(String.class)))
+//            .thenReturn(true);
+        BDDMockito.given(mailSendClient.send(any(String.class), any(String.class), any(String.class), any(String.class)))
+            .willReturn(true);
 
         //when
-        boolean result = mailService.send(fromEmail, toEmail, subject, content);
+        boolean result = mailService.send("", "", "", "");
 
         //then
+        verify(mailSendHistoryRepository, times(1)).save(any(MailSendHistory.class));
         assertThat(result).isTrue();
     }
 }
