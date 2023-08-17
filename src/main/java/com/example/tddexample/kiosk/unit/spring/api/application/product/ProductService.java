1b25f8363885d87135b5d6f8a2a6a4800837566b
hyunho058 <hyunho058@naver.com>
2023-08-17 10:45:43 +0900

    kiosk 상품 번호 생성 기능 분리(private 메서드 분리및 테스트 추가)

--- src/main/java/com/example/tddexample/kiosk/unit/spring/api/application/product/ProductService.java
+++ src/main/java/com/example/tddexample/kiosk/unit/spring/api/application/product/ProductService.java
@@ -2,7 +2,6 @@ package com.example.tddexample.kiosk.unit.spring.api.application.product;
 
 import com.example.tddexample.kiosk.unit.spring.api.application.product.dto.ProductCreateServiceRequest;
 import com.example.tddexample.kiosk.unit.spring.api.presentation.product.response.ProductResponse;
-import com.example.tddexample.kiosk.unit.spring.api.presentation.product.request.ProductCreateRequest;
 import com.example.tddexample.kiosk.unit.spring.domain.product.Product;
 import com.example.tddexample.kiosk.unit.spring.domain.product.ProductRepository;
 import com.example.tddexample.kiosk.unit.spring.domain.product.ProductSellingStatus;
@@ -18,6 +17,7 @@ import java.util.stream.Collectors;
 @Service
 public class ProductService {
     private final ProductRepository productRepository;
+    private final ProductNumberFactory productNumberFactory;
 
     public List<ProductResponse> getSellingProducts() {
         List<Product> products = productRepository.findAllBySellingStatusIn(ProductSellingStatus.forDisplay());
@@ -29,26 +29,11 @@ public class ProductService {
 
     @Transactional
     public ProductResponse createProduct(ProductCreateServiceRequest request) {
-        //productNumber
-        //001, 002, 003
-        //DB에서 마지막 저장된 product의 상품번호 조회해서 +1
-        String latestProductNumber = productRepository.findLatestProduct();
-
-        String nextProductNumber = createProductNumber(latestProductNumber);
+        String nextProductNumber = productNumberFactory.createProductNumber();
 
         Product newProduct = request.toEntity(nextProductNumber);
         Product savedProduct = productRepository.save(newProduct);
 
         return new ProductResponse(savedProduct);
-    }
-
-    private String createProductNumber(String latestProductNumber) {
-        if (latestProductNumber == null){
-            return "001";
-        }
-
-        int latestProductNumberInt = Integer.parseInt(latestProductNumber);
-        int nextProductNumberInt = latestProductNumberInt + 1;
-        return String.format("%03d", nextProductNumberInt);
     }
 }
