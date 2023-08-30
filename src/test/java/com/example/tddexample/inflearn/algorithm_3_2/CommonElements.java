9b3950f1bcbbd76f3e87e41120e33873e5bf1738
hyunho058 <hyunho058@naver.com>
2023-08-30 23:37:28 +0900

    algorithm 공통원소 구하기(포인터를 이용해 시간 복잡도O(n)으로 풀이)

---
+++ src/test/java/com/example/tddexample/inflearn/algorithm_3_2/CommonElements.java
@@ -0,0 +1,80 @@
+package com.example.tddexample.inflearn.algorithm_3_2;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class CommonElements {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] arr1 = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr1[i] = scanner.nextInt();
+        }
+
+        int m = scanner.nextInt();
+        int[] arr2 = new int[m];
+        for (int i = 0; i < m; i++) {
+            arr2[i] = scanner.nextInt();
+        }
+
+        CommonElements main = new CommonElements();
+        List<Integer> result = main.getDuplicates(arr1, arr2);
+
+        for (int i : result) {
+            System.out.print(i+" ");
+        }
+    }
+
+    public List<Integer> getDuplicates(int[] arr1, int[] arr2) {
+        List<Integer> result = new ArrayList<>();
+
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+
+        int arr1Idx = 0;
+        int arr2Idx = 0;
+
+        while (arr1Idx < arr1.length && arr2Idx < arr2.length) {
+            if (arr1[arr1Idx] < arr2[arr2Idx]) {
+                arr1Idx++;
+                continue;
+            }
+
+            if (arr1[arr1Idx] == arr2[arr2Idx]) {
+                result.add(arr1[arr1Idx]);
+                arr1Idx++;
+                arr2Idx++;
+                continue;
+            }
+
+            arr2Idx++;
+        }
+
+        return result;
+    }
+
+//    타임아웃
+//    public List<Integer> getDuplicates(int[] arr1, int[] arr2) {
+//        Arrays.sort(arr1);
+//        Arrays.sort(arr2);
+//
+//        List<Integer> result = new ArrayList<>();
+//
+//        for (int element : arr1) {
+//            int samePosition = 0;
+//
+//            for (int j = samePosition; j < arr2.length; j++) {
+//                if (element == arr2[j]) {
+//                    result.add(element);
+//                    break;
+//                }
+//            }
+//        }
+//
+//        return result;
+//    }
+}
