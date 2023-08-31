06ce7c7b1d4dace5a6228c1a6668c7096befabf6
hyunho058 <hyunho058@naver.com>
2023-08-31 11:22:10 +0900

    algorithm  연속된 자연수의 합

---
+++ src/test/java/com/example/tddexample/inflearn/algoritm_3_5/NaturalNumber.java
@@ -0,0 +1,48 @@
+package com.example.tddexample.inflearn.algoritm_3_5;
+
+public class NaturalNumber {
+    private final int number;
+
+    public NaturalNumber(int number) {
+        this.number = number;
+    }
+
+    public int calc() {
+        int result = 0;
+        int idx = this.getStartPosition();
+        int p2 = 1;
+
+        int sum = p2;
+
+        for (int p1 = 2; p1 <= idx; p1++) {
+            sum += p1;
+
+            if (sum == this.number) {
+                result++;
+                sum -= p2++;
+            }
+
+            while (sum > this.number) {
+                sum -= p2++;
+
+                if (sum == this.number) {
+                    result++;
+                }
+            }
+
+        }
+
+        return result;
+    }
+
+    private int getStartPosition() {
+        int startPosition = number / 2;
+
+        if (number % 2 != 0) {
+            startPosition += 1;
+        }
+
+        return startPosition;
+    }
+
+}
