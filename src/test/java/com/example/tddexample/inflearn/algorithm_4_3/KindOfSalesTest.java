b79bc01b7d01f42410b205067848411cf20b23c0
Hyunho Kim <hyunho058@naver.com>
2023-09-09 23:32:36 +0900

    algorithm 매출액의 종류 #8
    * algorithm 매출액의 종류

---
+++ src/test/java/com/example/tddexample/inflearn/algorithm_4_3/KindOfSalesTest.java
@@ -0,0 +1,42 @@
+package com.example.tddexample.inflearn.algorithm_4_3;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class KindOfSalesTest {
+
+
+    @Test
+    void test1() {
+        KindOfSales kindOfSales = new KindOfSales();
+        List<Integer> results = kindOfSales.sales(
+            7,
+            4,
+            new int[]{20, 12, 20, 10, 23, 17, 10}
+        );
+
+        assertThat(results).containsExactly(3, 4, 4, 3);
+    }
+
+    @Test
+    void test2() {
+        KindOfSales kindOfSales = new KindOfSales();
+        List<Integer> results = kindOfSales.sales(
+            20,
+            10,
+            new int[]{
+                365, 50, 50, 150, 60,
+                150, 154, 215, 150, 315,
+                293, 226, 408, 11, 344,
+                199, 313, 82, 394, 304
+            }
+        );
+
+        assertThat(results).containsExactly(7 ,7 ,8 ,8 ,9 ,9 ,10 ,10 ,10 ,10, 10);
+    }
+}
