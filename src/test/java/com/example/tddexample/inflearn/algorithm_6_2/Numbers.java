c093852d1439d770b462e09733c5c9d9d44d62db
hyunho058 <hyunho058@naver.com>
2023-09-13 15:24:59 +0900

    main 함수 추가

--- src/test/java/com/example/tddexample/inflearn/algorithm_6_2/Numbers.java
+++ src/test/java/com/example/tddexample/inflearn/algorithm_6_2/Numbers.java
@@ -1,6 +1,25 @@
 package com.example.tddexample.inflearn.algorithm_6_2;
 
+import java.util.Scanner;
+
 public class Numbers {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] ints = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            ints[i] = scanner.nextInt();
+        }
+
+        Numbers main = new Numbers();
+        int[] result = main.sort(ints);
+
+        for (int i : result) {
+            System.out.println(i);
+        }
+    }
 
     public int[] sort(int[] numbers) {
 
