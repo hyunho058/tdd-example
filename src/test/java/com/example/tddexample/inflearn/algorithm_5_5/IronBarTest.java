0766b5db3cccf1105189dbb35ab5f036dfb8a653
hyunho058 <hyunho058@naver.com>
2023-09-15 11:35:44 +0900

    algorithm 쇠막대기

---
+++ src/test/java/com/example/tddexample/inflearn/algorithm_5_5/IronBarTest.java
@@ -0,0 +1,33 @@
+package com.example.tddexample.inflearn.algorithm_5_5;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class IronBarTest {
+
+    /**
+     * ()(((()())(())()))(())
+     * () ( ( (()()) (()) () ) ) (())
+     * ()(((()())(())()))(())
+     * ((()))
+     */
+
+    @DisplayName("열고 닫는 괄호가 연속으로 오는 한쌍이면 레이저 이다.")
+    @Test
+    void test1() {
+        IronBar ironBar = new IronBar();
+        int result = ironBar.amputation("(()())");
+
+        assertThat(result).isEqualTo(3);
+    }
+
+    @Test
+    void test2() {
+        IronBar ironBar = new IronBar();
+        int result = ironBar.amputation("()(((()())(())()))(())");
+
+        assertThat(result).isEqualTo(17);
+    }
+}
