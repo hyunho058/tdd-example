493eefe9409638da7d4fbbb0c84d8b71438bfa29
Hyunho Kim <hyunho058@naver.com>
2023-10-05 21:23:20 +0900

    algorithm LRU #20
    * algorithm LRU
    
    * algorithm LRU

---
+++ src/test/java/com/example/tddexample/inflearn/algortihm_6_4/Memory.java
@@ -0,0 +1,68 @@
+package com.example.tddexample.inflearn.algortihm_6_4;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Memory {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int s = scanner.nextInt();
+        int n = scanner.nextInt();
+        int[] ints = new int[n];
+        for (int i = 0; i < n; i++) {
+            ints[i] = scanner.nextInt();
+        }
+
+        Memory main = new Memory();
+        List<Integer> result = main.leastRecentlyUsed(s, ints);
+
+        for (int i  : result) {
+            System.out.print(i + " ");
+        }
+    }
+
+    public List<Integer> leastRecentlyUsed(int cacheSize, int[] jobs) {
+        LinkedList<Integer> cache = new LinkedList<>();
+        for (int job : jobs) {
+            if (cache.isEmpty()) {
+                cache.add(job);
+                continue;
+            }
+
+            for (int j = 0; j < cache.size(); j++) {
+                if (job == cache.get(j)) {
+                    cache.remove(j);
+                    break;
+                }
+            }
+
+            cache.add(0, job);
+
+            if (cache.size() > cacheSize) {
+                cache.remove(cache.size() - 1);
+            }
+        }
+
+        return cache;
+    }
+
+    static class Cache {
+        private final LinkedList<Integer> memory;
+
+        public Cache() {
+            this(new LinkedList<Integer>());
+        }
+
+        public Cache(LinkedList<Integer> memory) {
+            this.memory = memory;
+        }
+
+        public boolean isEmpty() {
+            return false;
+        }
+
+    }
+}
