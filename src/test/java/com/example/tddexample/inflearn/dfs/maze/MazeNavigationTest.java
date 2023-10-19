922bc8637788994dddf6cb7ae5a63ec78d480919
hyunho058 <hyunho058@naver.com>
2023-10-19 22:48:33 +0900

    모든 경로 찾기

---
+++ src/test/java/com/example/tddexample/inflearn/dfs/maze/MazeNavigationTest.java
@@ -0,0 +1,28 @@
+package com.example.tddexample.inflearn.dfs.maze;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MazeNavigationTest {
+    @Test
+    void test() {
+        int[][] board = {
+
+            {0, 0, 0, 0, 0, 0, 0},
+            {0, 1, 1, 1, 1, 1, 0},
+            {0, 0, 0, 1, 0, 0, 0},
+            {1, 1, 0, 1, 0, 1, 1},
+            {1, 1, 0, 0, 0, 0, 1},
+            {1, 1, 0, 1, 1, 0, 0},
+            {1, 0, 0, 0, 0, 0, 0}
+        };
+        MazeNavigation maze = new MazeNavigation(board);
+        board[0][0] = 1;
+        maze.dfs(0, 0);
+
+        Assertions.assertThat(maze.getAnswer()).isEqualTo(8);
+    }
+}
