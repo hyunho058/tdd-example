116028c6cd82d373ad75ea8408cb3785087d8fe1
hyunho058 <hyunho058@naver.com>
2023-11-01 22:34:03 +0900

    최대점수 구하기(냅색 알고리즘)

---
+++ src/test/java/com/example/tddexample/inflearn/dynamic/score/ScoreTest.java
@@ -0,0 +1,28 @@
+package com.example.tddexample.inflearn.dynamic.score;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class ScoreTest {
+    @DisplayName("")
+    @Test
+    void test() {
+        int n = 5;
+        int timeLimit = 20;
+        int[][] problemArr = {
+            {10, 5},
+            {25, 12},
+            {15, 8},
+            {6, 3},
+            {7, 4}
+        };
+
+        Score score = new Score(n ,timeLimit, problemArr);
+        int result = score.maxScore();
+
+        assertThat(result).isEqualTo(41);
+    }
+}
